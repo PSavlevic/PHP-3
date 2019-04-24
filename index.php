e86152e22934ade0bb46f7ea155b57725bb32721
Pavel <p.savlevic@gmail.com>
2019-04-24 09:45:38 +0300

    4 uzd (FOR)

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
-
-$months = 12;
-$kisene = 1000;
-$alga = 700;
-
-for ($months = 0; $months < 12; $months++) {
-    $islaidos = rand(400, 1200);
-    $balansas = $kisene + $alga - $islaidos;
-
-    if ($balansas <= 0) {
-        print "Tau pzdc per $months menesiu";
-        break;
+$pakelio_kaina = 3.75;
+$surukyta = 0;
+$surukyta2 = 0;
+for ($day = 1; $day <= 365; $day++) {
+    $weekday = date('N', strtotime("+$day days"));
+    if ($weekday <= 4) {
+        $cizos_mon_thu = rand(3, 4); // Nuo pirmadienio iki ketvirtadienio
+        $surukyta += $cizos_mon_thu; // Kiek surūkiau iki ketvirtadienio
+        $surukyta2 += $cizos_mon_thu; // Kiek surūkiau iki ketvirtadienio
+    } elseif ($weekday == 5) {
+        $cizos_fri = rand(10, 20); // Penktadienis
+        $surukyta += $cizos_fri; // Kiek surukiau per penktadieni
     } else {
-        $kisene = $balansas;
+        $cizos_sat_sun = rand(1, 3); // Sestadienis-sek
+        $surukyta += $cizos_sat_sun; // Kiek surukiau per sest-sek
     }
 }
-
-$text = "likutis $kisene";
+$kainuos = round($surukyta / 20 * 3.75);
+$text = "Per metus surukysiu $surukyta cigareciu uz $kainuos eur.";
+$kainuos2 = round($surukyta2 / 20 * 3.75);
+$text2 = "Nerukydamas pirmadieni-ketvirtaideni sutaupyciau $kainuos2 eur.";
 ?>
-<!DOCTYPE html>
 <html>
 <head>
-    <title>20190423</title>
+    <title>main</title>
 </head>
 <body>
-<section>
-    <p>
-        <?php print $text; ?>
-    </p>
-</section>
+<P>
+    <?php print $text . ' ' . $text2;?>
+</P>
 </body>
 </html>
