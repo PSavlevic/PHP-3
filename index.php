6c41ce141febdb0158290c56cc04c66e1058cccf
Pavel <p.savlevic@gmail.com>
2019-04-24 16:45:32 +0300

    Masinos nuvertejimas per 24men (FOR)

--- index.php
+++ index.php
@@ -1,54 +1,25 @@
 <?php
-$pakelio_kaina = 3.75;
-$surukyta = 0;
-$surukyta2 = 0;
-$viso_min = 0;
-$rukymo_laikas = 5; // 5 uzduotis
 
-for ($day = 1; $day <= 365; $day++) {
-    $weekday = date('N', strtotime("+$day days"));
-    if ($weekday <= 4) {
-        $cizos_mon_thu = rand(3, 4); // Nuo pirmadienio iki ketvirtadienio
-        $surukyta += $cizos_mon_thu; // Kiek surūkiau iki ketvirtadienio
-        $surukyta2 += $cizos_mon_thu; // Kiek surūkiau iki ketvirtadien
-    } elseif ($weekday == 5) {
-        $cizos_fri = rand(10, 20); // Penktadienis
-        $surukyta += $cizos_fri; // Kiek surukiau per penktadieni
-    } else {
-        $cizos_sat_sun = rand(1, 3); // Sestadienis-sek
-        $surukyta += $cizos_sat_sun; // Kiek surukiau per sest-sek
-    }
-    $viso_min = $surukyta * $rukymo_laikas; // 5 uzduotis
+$months = 24;
+$car_price_new = 30000;
+$car_price_left = $car_price_new;
+
+for ($months; $months > 0; --$months) {
+    $car_price_left -= $car_price_left * 0.02;
+//    print $car_price_left . '<br>';
 }
-$viso_hours = round($viso_min / 60);   // 5 uzduotis
 
-$kainuos = round($surukyta / 20 * 3.75);
-$text = "Per metus surukysiu $surukyta cigareciu uz $kainuos eur.";
-$kainuos2 = round($surukyta2 / 20 * 3.75);
-$text2 = "Nerukydamas pirmadieni-ketvirtaideni sutaupyciau $kainuos2 eur.";
-$text3 = "Per metus prastovesiu $viso_hours valandu traukdamas"; // 5 uzduotis
+$depreciated = 100 - (($car_price_left / $car_price_new)*100);
+$text = "Po 24 menesiu masinos verte bus: $car_price_left ir nuvertejo $depreciated procentu.";
 
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>main</title>
-    <style>
-        img {
-            width: 30px;
-            height: 30px;
-        }
-    </style>
+    <title>ciklai</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-<p>
-    <?php print $text . ' ' . $text2 . '<br> ' . $text3 . '<br><br>';?>
-</p>
-
-<div class="container">
-    <?php for ($n = 0; $n < $surukyta; $n++):?>
-        <img src="https://image.flaticon.com/icons/svg/1673/1673015.svg" alt="cig">
-    <?php endfor; ?>
-</div>
-
+<p><?php print $text; ?></p>
 </body>
 </html>
