01fe59665a225286f9d1ffd25fda3f3a9656303d
Pavel <p.savlevic@gmail.com>
2019-04-25 16:00:20 +0300

    Uzduotis 2 (ARRAY) Clean Code

--- index.php
+++ index.php
@@ -1,23 +1,42 @@
 <?php
-//ARRAY2
 
-$saldytuvas = [
-
-    'produktai' => 'du dideli kiausiniai', 'vienas mazas baklazanas',
-    'indas1' => 'vienas didelis grietines indelis'
-    
+$arr = [
+    'kiausiniai' => [
+        'name' => 'Kiaušinis',
+        'amount' => 1,
+        'size' => 'didelis',
+    ],
+    'baklazanai' => [
+        'name' => 'Baklažanas',
+        'amount' => 1,
+        'size' => 'mazas',
+    ],
+    'grietine' => [
+        'name' => 'Grietinė',
+        'amount' => 1,
+        'size' => 'mazas',
+    ]
 ];
 
-var_dump($saldytuvas);
+var_dump($arr);
 
 ?>
-<!DOCTYPE html>
 <html>
 <head>
-    <title>ciklai</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <title>Array</title>
 </head>
 <body>
-
+<div class="produktas">
+            <span class="pavadinimas">
+                <?php print $arr['grietine']['name']; ?>
+            </span>
+    <span class="kiekis">
+                <?php print $arr['grietine']['amount']; ?>
+            </span>
+    <span class="dydis">
+                <?php print $arr['grietine']['size']; ?>
+            </span>
+</div>
 </body>
 </html>
