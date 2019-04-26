34205c79e013b7feba815c00c17b65a08b0631bb
Pavel <p.savlevic@gmail.com>
2019-04-26 11:46:31 +0300

    Tado 1 uzd (nepilnametis/pilnametis/pensininkas)

--- index.php
+++ index.php
@@ -1,41 +1,30 @@
 <?php
 
-$mano_atmintis = [
-    'Penktadienis',
-    'Paskaita',
-    'Baras',
-    'Viskis',
-    'Alus',
-    'Degtine',
-    'Alus',
-    'Pirmadienis',
-    'Paskaita'
-];
+function amzius($amzius) {
+    if ($amzius < 0) {
+        return 'klaida';
+    }
+    if ($amzius <= 18) {
+        return 'nepilnametis';
+    } if ($amzius < 65) {
+        return 'pilnametis';
+    } if (66 < $amzius) {
+        return 'pensininkas';
+    }
+}
 
-$rand_flashback = rand(0, count($mano_atmintis) - 1);
+print amzius(120);
+
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Klases daras</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>ciklai</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
 </head>
 <body>
-<h1>Kas buvo penktadienį ?!</h1>
-<h2>Mano atmintis</h2>
-<p>
-<ul> <!-- foreach -->
-    <?php foreach($mano_atmintis as $value): ?>
-        <li>
-            <?php print $value; ?>
-        </li>
-    <?php endforeach; ?>
-</ul><!-- endforeach -->
-</p>
-<?php
-print '#' .$rand_flashback . ': ' . $mano_atmintis[$rand_flashback] ;
-?>
+
 </body>
 </html>
