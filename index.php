f98b93852c1ca48291a22bf609f97b3a8a0f11b8
Pavel <p.savlevic@gmail.com>
2019-04-29 16:31:17 +0300

    uzd 2

--- index.php
+++ index.php
@@ -19,14 +19,24 @@ $ataskaita = [
     ],
 ];
 
+$visos_islaidos = 0;
+$visos_iplaukos = 0;
+$balansas = 0;
+
 foreach ($ataskaita as $index => $irasas) {
     if ($irasas['amount'] > 0) {
         $css_class = 'positive';
+        $visos_iplaukos += $irasas['amount'];
     } else {
         $css_class = 'negative';
+        $visos_islaidos -= $irasas['amount'];
     }
+
     $ataskaita[$index]['css_class'] = $css_class;
+    $balansas += $irasas['amount'];
 }
+
+$text = "Balansas yra lygu $balansas, Visos islaidos $visos_islaidos ,o iplaukos yra lygu $visos_iplaukos."
 
 ?>
 <!DOCTYPE html>
@@ -45,13 +55,16 @@ foreach ($ataskaita as $index => $irasas) {
     </style>
 </head>
 <body>
-<ul>
-    <?php foreach ($ataskaita as $irasas): ?>
-        <li class="<?php print $irasas['css_class']; ?>">
-            <span><?php print $irasas['name']; ?></span>
-            <span><?php print $irasas['amount']; ?></span>
-        </li>
-    <?php endforeach; ?>
-</ul>
+    <ul>
+        <?php foreach ($ataskaita as $irasas): ?>
+            <li class="<?php print $irasas['css_class']; ?>">
+                <span><?php print $irasas['name']; ?></span>
+                <span><?php print $irasas['amount']; ?></span>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+    <p>
+        <?php print $text; ?>
+    </p>
 </body>
 </html>
