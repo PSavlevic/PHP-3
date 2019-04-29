e859425a4fe3e36fd853135b85b4339d7b7013d3
Pavel <p.savlevic@gmail.com>
2019-04-29 15:49:52 +0300

    Uzd 1 (array/foreach) 2019.04.29 (css class positive/negative)

--- index.php
+++ index.php
@@ -2,36 +2,55 @@
 
 $ataskaita = [
     [
-        'name' => 'Iki Darbo Uzmokestis',
-        'amount' => 600,
+        'name' => 'IKI Darbo Uzmokestis',
+        'amount' => '600',
     ],
-
     [
         'name' => 'Kalvariju Nacnykas',
-        'amount' => -15,
-    ],
-
-    [
-        'name' => 'Vilniaus baras',
-        'amount' => -50,
+        'amount' => '-15',
     ],
     [
-        'name' => 'Casino',
-        'amount' => -150,
+        'name' => 'Limonas',
+        'amount' => '-120',
+    ],
+    [
+        'name' => 'Vovkos garazas',
+        'amount' => '-340',
     ],
 ];
 
-var_dump($ataskaita);
-?>
+foreach ($ataskaita as $index => $irasas) {
+    if ($irasas['amount'] > 0) {
+        $ataskaita[$index]['css_class'] = 'positive';
+    } else {
+        $ataskaita[$index]['css_class'] = 'negative';
+    }
+}
 
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Ataskaita</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        .positive {
+            color: green;
+        }
+        .negative {
+            color: red;
+        }
+    </style>
 </head>
 <body>
-
+<ul>
+    <?php foreach ($ataskaita as $irasas): ?>
+        <li class="<?php print $irasas['css_class']; ?>">
+            <span><?php print $irasas['name']; ?></span>
+            <span><?php print $irasas['amount']; ?></span>
+        </li>
+    <?php endforeach; ?>
+</ul>
 </body>
 </html>
