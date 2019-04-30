be8ff5cdb03aac8a78a7b2dcf96c715a9991b193
Pavel <p.savlevic@gmail.com>
2019-04-30 13:50:57 +0300

    <= 4 uzd (FULL)

--- index.php
+++ index.php
@@ -1,32 +1,28 @@
 <?php
-
-$daiktai = [ 'Kremas', 'Riesutai', 'Lupdazis', 'Kojines'
+$rasinys =[];
+$zodziai = [
+    'lauke', 'ryte', 'prie', 'Maxima', 'masina',
 ];
-$size = rand(1, 10);
-$is_dark = rand(0, 1);
-$rand_name = rand(0, count($daiktai)-1);
-
-//randomina tamsius/sviesus
-if ($is_dark) {
-    $color = 'tamsus';
-} else {
-    $color = 'sviesus';
+$rasinio_ilgis = rand(100, 300);
+for ($katiusa=0; $katiusa < $rasinio_ilgis; $katiusa++) {
+    $random_index = rand(0, count($zodziai)-1);
+    $random_zodis = $zodziai[$random_index];
+    $rasinys[] = $random_zodis;
 }
-//spausdinamas tekstas
-$text = "$daiktai[$rand_name] uzima $size cm3. Daiktas $color";
-
+//var_dump($rasinys);
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Ataskaita</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<p>
-    <?php print $text; ?>.
-</p>
+    <ul>
+        <?php foreach ($rasinys as $value): ?>
+            <li> <?php print $value; ?></li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
