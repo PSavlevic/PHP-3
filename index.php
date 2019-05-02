d4a07e753f23f298c5db7a7e91ee69c505a0a549
Pavel <p.savlevic@gmail.com>
2019-05-02 16:51:18 +0300

    4 uzd (clean Code)

--- index.php
+++ index.php
@@ -1,52 +1,78 @@
 <?php
+
 /**
- * @return  pavaizduotas html'e sugeneruotas arrejus
+ * Generuoja dinaminę matricą
+ * @param integer $size Matricos dydis
+ * @return array
  */
-function slot_run($size)
-{
+function slot_run($size) {
     $array = [];
 
     for ($x = 1; $x <= $size; $x++) {
         $row = [];
 
-        for ($y = 1; $y <= $size; $y++) {
+        for ($z = 1; $z <= $size; $z++) {
             $row[] = rand(0, 1);
         }
+
         $array[] = $row;
     }
+
     return $array;
 }
 
-$array = slot_run(7);
+function winning_slot($matrix){
+    $winning_rows = [];
 
-var_dump(slot_run(5));
+    foreach ($matrix as $index => $row){
+        $row_winning_sum = count($row);
+        $column_sum = 0;
+
+        foreach ($row as $column){
+            $column_sum += $column;
+        }
+        if ($row_winning_sum == $column_sum){
+            $winning_rows[] = $index;
+        }
+        if ($column_sum == 0){
+            $winning_rows[] = $index;
+        }
+    }
+
+    return $winning_rows;
+}
+
+//var_dump(slot_run(10));
+$array = slot_run(3);
+$winning_rows = winning_slot($array);
+var_dump($winning_rows);
 
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Klasės darbas, 05.02</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <style>
-        section div {
+        div {
             border: 3px solid black;
             padding: 20px;
             margin: 20px;
+            height: 5vh;
+            width: 5vh;
         }
-
         section {
             display: flex;
             justify-content: center;
             align-items: baseline;
         }
 
-        .blue {
+        .yellow {
             background-color: blue;
         }
 
-        .orange {
+        .green {
             background-color: orange;
         }
     </style>
@@ -54,11 +80,11 @@ var_dump(slot_run(5));
 <body>
 <?php foreach ($array as $index => $value): ?>
     <section>
-        <?php foreach ($value as $index => $valuee): ?>
-            <?php if ($valuee): ?>
-                <div class="blue"></div>
+        <?php foreach ($value as $index => $random): ?>
+            <?php if ($random): ?>
+                <div class="yellow"></div>
             <?php else: ?>
-                <div class="orange"></div>
+                <div class="green"></div>
             <?php endif; ?>
         <?php endforeach; ?>
     </section>
