f39cff05cc67fddf82f1879f46d2c31a2614fe00
Pavel <p.savlevic@gmail.com>
2019-05-02 11:11:40 +0300

    FUNCTION homework 5 uzd (2019.05.02)

--- index.php
+++ index.php
@@ -2,31 +2,56 @@
 /**
  * @return array 3x3 random array
  */
-
-function slot_run() {
-   $array = [
-       [rand(0, 1), rand(0, 1), rand(0, 1)],
-       [rand(0, 1), rand(0, 1), rand(0, 1)],
-       [rand(0, 1), rand(0, 1), rand(0, 1)]
-   ];
-   return($array);
+function slot_run()
+{
+    $array = [
+        [rand(0, 1), rand(0, 1), rand(0, 1)],
+        [rand(0, 1), rand(0, 1), rand(0, 1)],
+        [rand(0, 1), rand(0, 1), rand(0, 1)]
+    ];
+    return $array;
 }
 
-var_dump(slot_run());
+$array1 = slot_run();
 
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="1">
-    <title>title</title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/grid.css">
-    <link rel="stylesheet" href="css/style.css">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        section div {
+            border: 3px solid black;
+            padding: 20px;
+            margin: 20px;
+        }
+        section {
+            display: flex;
+            justify-content: center;
+            align-items: baseline;
+        }
+        .blue {
+            background-color: blue;
+        }
+        .orange {
+            background-color: orange;
+        }
+    </style>
 </head>
 <body>
-
-
+<?php foreach ($array1 as $index): ?>
+    <section>
+        <?php foreach ($index as $value): ?>
+            <?php if ($value > 0): ?>
+                <div class="blue"></div>
+            <?php else: ?>
+                <div class="orange"></div>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </section>
+<?php endforeach; ?>
 </body>
 </html>
