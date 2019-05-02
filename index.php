c999058eb58aec2a302b9fbf848cd6b78114e89a
Pavel <p.savlevic@gmail.com>
2019-05-02 09:41:54 +0300

    FUNCTION homework 4 uzd (paprastesnis variantas)

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
+/**
+ * @return array 3x3 random array
+ */
 
-$array = [12, 43, 654, 23];
-function avg($array) {
-    $sum = 0;
-    foreach ($array as $value) {
-        $sum += $value;
-    }
-    return ($sum/count($array));
+function slot_run() {
+   $array = [
+       [rand(0, 1), rand(0, 1), rand(0, 1)],
+       [rand(0, 1), rand(0, 1), rand(0, 1)],
+       [rand(0, 1), rand(0, 1), rand(0, 1)]
+   ];
+   return($array);
 }
-print avg($array);
+
+var_dump(slot_run());
 
 ?>
 <!DOCTYPE html>
