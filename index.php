9d45c04b8ffa116e9d36d60066fdf954d35766cc
Pavel <p.savlevic@gmail.com>
2019-05-02 13:24:09 +0300

    str_replace (keiksmazodziai tekste) - (2019.05.02)

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
-/**
- * @return array 3x3 random array
- */
-function slot_run()
-{
-    $array = [
-        [rand(0, 1), rand(0, 1), rand(0, 1)],
-        [rand(0, 1), rand(0, 1), rand(0, 1)],
-        [rand(0, 1), rand(0, 1), rand(0, 1)]
-    ];
-    return $array;
+function tinkamis_zodis($text) {
+    return str_replace('blet', '****', $text);
 }
-
-$array1 = slot_run();
 
 ?>
 
@@ -22,36 +11,8 @@ $array1 = slot_run();
     <meta charset="UTF-8">
     <title>Title</title>
     <link rel="stylesheet" href="style.css">
-    <style>
-        section div {
-            border: 3px solid black;
-            padding: 20px;
-            margin: 20px;
-        }
-        section {
-            display: flex;
-            justify-content: center;
-            align-items: baseline;
-        }
-        .blue {
-            background-color: blue;
-        }
-        .orange {
-            background-color: orange;
-        }
-    </style>
 </head>
 <body>
-<?php foreach ($array1 as $index): ?>
-    <section>
-        <?php foreach ($index as $value): ?>
-            <?php if ($value > 0): ?>
-                <div class="blue"></div>
-            <?php else: ?>
-                <div class="orange"></div>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    </section>
-<?php endforeach; ?>
+    <?php print tinkamis_zodis('Contrary to popular blet belief,  of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'); ?>
 </body>
 </html>
