c4efe0c123f55cd8fbdd21f2f43f45bdfadbf446
Pavel <p.savlevic@gmail.com>
2019-05-06 11:29:04 +0300

    Function ((ne)pilnamatis, gender) -konsultacijos

--- index.php
+++ index.php
@@ -1,29 +1,39 @@
 <?php
 
-function duok($arrayKey, $colorKey){
-
-    $array = [
-        'labas',
-        'krabas',
-        'matas',
-    ];
-
-    $color = [
-        'melyna',
-        'raudona',
-        'ruda',
-    ];
-
-    foreach ($array as $key => $value){
-        $tekstas =  $array[$arrayKey];
+function pilnametis ($name, $age) {
+    if ($age >= 18) {
+        print $name . ' pilnametis';
+    } else {
+        print $name . ' nepilnametis';
     }
-
-    foreach ($color as $key => $value){
-        $spalva =  $color[$colorKey];
-    }
-    return $tekstas . ' ' . $spalva;
 }
 
-print duok(2,1);
+pilnametis('Darius', 19);
+
+function nupirktAlaus($age, $gender) {
+    if ($age >= 20) {
+        print 'pilnametis ir gali nupirkti';
+        if ($gender == 'man') {
+            print ' Gausi alaus!';
+        } else {
+            print ' gal gausi alaus...';
+        }
+    } else {
+        print ' nepilnametis ir gali nupirkti';
+    }
+}
+
+print nupirktAlaus(25, 'man');
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>title</title>
+</head>
+<body>
+
+
+</body>
+</html>
