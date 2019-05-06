c8629af2c3984d33dc2d56443b78c42d1fe7b17e
Pavel <p.savlevic@gmail.com>
2019-05-06 11:31:06 +0300

    Function ((ne)pilnamatis, gender) -konsultacijos

--- index.php
+++ index.php
@@ -8,22 +8,22 @@ function pilnametis ($name, $age) {
     }
 }
 
-pilnametis('Darius', 19);
+//pilnametis('Darius', 19);
 
 function nupirktAlaus($age, $gender) {
     if ($age >= 20) {
-        print 'pilnametis ir gali nupirkti';
+        print 'pilnametis ir gali nupirkti!';
         if ($gender == 'man') {
-            print ' Gausi alaus!';
+            print ' Tikrai gausi alaus, nes esi vyras!';
         } else {
-            print ' gal gausi alaus...';
+            print ' Gal gausi alaus o gal ir ne... nes esi moteris';
         }
     } else {
         print ' nepilnametis ir gali nupirkti';
     }
 }
 
-print nupirktAlaus(25, 'man');
+print nupirktAlaus(25, 'woman');
 
 ?>
 <!DOCTYPE html>
