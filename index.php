d8a18a5da2e540b3b5b8f279994d27745d1217e0
Pavel <p.savlevic@gmail.com>
2019-05-07 16:30:00 +0300

    3 uzd (&) v1

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
 
-$x = 0;
-$b = &$x;
-unset($b);
-$b = 1;
+$roll_joints = true;
+$joint1 = false;
+$joint2 = false;
+$joint3 = false;
+$joint1 = &$roll_joints;
+$joint2 = &$joint1;
+$joint3 = &$joint2;
 
-print $x;
+print $joint1;
+print $joint2;
+print $joint3;
 
 ?>
