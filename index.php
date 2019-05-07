2011aa5811f071193a9c429be73715c023ac5b2e
Pavel <p.savlevic@gmail.com>
2019-05-07 17:27:30 +0300

    5 uzd (&) v1

--- index.php
+++ index.php
@@ -6,4 +6,11 @@ for ($x = 0; $x < 5; $x++){
     $sheep[] = &$sheep[$x];
 }
 
+foreach ($sheep as $key => $value) {
+    unset($sheep[$key]);
+    $sheep[$key] = $value;
+}
+
+$sheep[2] = 'au au au';
+
 var_dump($sheep);
