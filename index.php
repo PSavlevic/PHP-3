a132e47b5c826979bcb35b7e74e0b545c27127c6
Pavel <p.savlevic@gmail.com>
2019-05-07 16:24:07 +0300

    2 uzd (&) v2

--- index.php
+++ index.php
@@ -2,22 +2,9 @@
 
 $x = 0;
 $b = &$x;
-
 unset($b);
-
 $b = 1;
 
 print $x;
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>title</title>
-</head>
-<body>
-
-
-</body>
-</html>
