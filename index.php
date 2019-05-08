6851a420fdc9047432884912b11d14d211049b9e
Pavel <p.savlevic@gmail.com>
2019-05-08 17:07:34 +0300

    3 uzd (form) img spausdinamas tiek kartu, koks yra skaicius buttone

--- index.php
+++ index.php
@@ -18,5 +18,9 @@ if (!empty ($_POST)) {
     <form action="index.php" method="post">
         <button name="patvirtinimas" value="<?php print $result; ?>"><?php print $result; ?></button>
     </form>
+    <?php for ($x = 0; $x < $result; $x++): ?>
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/000/436/882/small/Food__28466_29.jpg?1555154176?nocache=true&cache_buster=47420021-b716-44c8-a3c2-cff4d975dda2"
+             alt="">
+    <?php endfor; ?>
 </body>
 </html>
