78238ff7f4338cb35dcca5cedc15ce5b403ce65f
Pavel <p.savlevic@gmail.com>
2019-05-09 17:09:53 +0300

    3 uzd (security FORM)

--- index.php
+++ index.php
@@ -1,24 +1,54 @@
 <?php
 
-$hack = filter_input_array (INPUT_POST, [
-    'hack_me' => FILTER_SANITIZE_SPECIAL_CHARS,
-    ]);
+var_dump($_POST);
 
-if (!empty($hack)) {
-    $text = $hack['hack_me'];
-} else {
-    $text = 'Ivesk kazka';
-}
+$form = [
+    'fields' => [
+        'name' => [
+            'label' => 'Mano vardas',
+            'type' => 'text',
+            'placeholder' => 'Vardas'
+        ],
+        'zirniai' => [
+            'label' => 'Kiek turiu žirnių?',
+            'type' => 'text',
+            'placeholder' => '1-100'
+        ],
+        'reason' => [
+            'label' => 'Paslaptis, kodėl turiu žirnių',
+            'type' => 'password',
+            'placeholder' => 'Išsipasakok'
+        ]
+    ],
+    'buttons' => [
+        'do_zirniai' => [
+            'label' => 'paberti',
+        ],
+        'do_grikiai' => [
+            'label' => 'paberti',
+        ]
+    ]
+];
 
-
-//var_dump($hack);
 ?>
 <html>
-    <body>
-    <h1><?php print $text; ?></h1>
-        <form method="post">
-            <input type="text" name="hack_me" placeholder="Hack Me" required>
-            <button name="button" value="push">Try</button>
-        </form>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>Forms</title>
+    <link rel="stylesheet" type="text/css" href="include/normalise.css">
+    <link rel="stylesheet" type="text/css" href="include/style.css">
+</head>
+<body>
+<form method="POST">
+    <?php foreach ($form['fields'] as $field_id => $field): ?>
+        <label>
+            <span><?php print $field['label']; ?></span>
+            <input type="<?php print $field['type']; ?>" name="<?php print $field_id; ?>" placeholder="<?php print $field['placeholder']; ?>">
+        </label>
+    <?php endforeach; ?>
+    <?php foreach ($form['buttons'] as $button_id => $button): ?>
+        <button name="action" value="<?php print $button_id; ?>"><?php print $button['label']; ?></button>
+    <?php endforeach; ?>
+</form>
+</body>
 </html>
