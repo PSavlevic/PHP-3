5fac881c7e8f4f6e4d05c0f2524acda0673e56a2
Pavel <p.savlevic@gmail.com>
2019-05-14 16:07:28 +0300

    Google puslapis 05.14

--- index.php
+++ index.php
@@ -3,12 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <title>title</title>
-
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/grid.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
- <p>testas</p>
+    <h1>HTML revision</h1>
+    <h2>Centering HTML Elements</h2>
+    <section class="raudonas">
+        <div class="google_div">
+            <img src="https://image.flaticon.com/teams/slug/google.jpg" alt="google">
+        </div>
+    </section>
+    <section>
+        <h2 class="h2_margin">Text Alignment</h2>
+        <div class="left">Hello</div>
+        <div class="center">Hello</div>
+        <div class="right">Hello</div>
+    </section>
+
+    <h2 class="h2_margin">Box model</h2>
+    <section class="box_model">
+        <div class="melynas">
+            <div class="div_hello">Hello</div>
+            <div class="div_bye">Bye</div>
+        </div>
+    </section>
+    <h2 class="h2_margin">Playing with Inline Box Model</h2>
+    <section>
+        <div>
+            <img class="google_icon" src="https://image.flaticon.com/teams/slug/google.jpg" alt="icon">
+            <img class="google_icon" src="https://image.flaticon.com/teams/slug/google.jpg" alt="icon">
+            <img class="google_icon" src="https://image.flaticon.com/teams/slug/google.jpg" alt="icon">
+        </div>
+        <div>
+            <a href="#">Link1</a>
+            <a href="#">Link2</a>
+            <a href="#">Link3</a>
+        </div>
+    </section>
+    <h2 class="h2_margin">Playing with Block Margin</h2>
+    <div class="red"></div>
+    <div class="red"></div>
+    <div class="red"></div>
 </body>
 </html>
